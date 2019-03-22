f2a9f64ea9f037b47347bca54461208783453b8d
github username <metelskyi88@gmail.com>
2019-03-22 09:32:39 -0400

    front end set up

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount(){
+    fetch('http://localhost:3000/fish')
+    .then(res => res.json())
+    .then(fishData => console.log(fishData))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
