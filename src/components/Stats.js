809e5616a16a22f5b6346a91f072f346a17c9d8a
github username <github email address>
2019-03-25 11:59:03 -0400

    game stats are shown on fight page

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
-const Stats = () => {
+const Stats = (props) => {
   return (
     <div>
-    {'Stats'}
+      <Paper className='stats' elevation={2}>
+        <Typography variant="h5" component="h3">
+          {props.name}
+        </Typography>
+        <GridList cellHeight={30} >
+        <GridListTile>
+          <Chip avatar={<Avatar>HP</Avatar>} label={props.hp} />
+        </GridListTile>
+        <GridListTile>
+          <Chip avatar={<Avatar>PWR</Avatar>} label={props.power} />
+        </GridListTile>
+        </GridList>
+      </Paper>
     </div>
   )
 }
