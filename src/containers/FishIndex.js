d058006588873d5de25e883007bb5bacd9baeaae
github username <metelskyi88@gmail.com>
2019-03-26 17:32:21 -0400

    opponent selection works

--- src/containers/FishIndex.js
+++ src/containers/FishIndex.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import FishCard from '../components/FishCard'
 import Grid from '@material-ui/core/Grid';
-
+import OpponentCard from '../components/OpponentCard'
+import Card from '@material-ui/core/Card'
+import ListSubheader from '@material-ui/core/ListSubheader'
 
 
 class FishIndex extends React.Component {
 
   render(){
     return (
+      <React.Fragment>
       <Grid container justify="center" >
         {this.props.fish.map(fish => <FishCard deleteFish={this.props.deleteFish} clickFish={this.props.clickCard} key={fish.id} fish={fish}/>)}
-    </Grid>)
+    </Grid>
+    <h3 justify='flex-start'>Select An Opponent:</h3>
+    <Grid container justify="center">
+    {this.props.opponents.map(fish => <Card><OpponentCard fish={fish} handleSelectOpponent={this.props.handleSelectOpponent}/> </Card>)}
+    </Grid>
+  </React.Fragment>)
   }
 }
 
