5ca5af03934c5cad69d8f697932c5103d73d5f5a
github username <github email address>
2019-03-26 18:29:45 -0400

    changed order of health and power on opponents to be consistent

--- src/components/OpponentCard.js
+++ src/components/OpponentCard.js
@@ -8,8 +8,8 @@ return (
   <div className='opponent-card' onClick={(event) => props.handleSelectOpponent(props.fish, event)}>
     <img className='image' alt='' src={require(`../beta_images/${fish.img_url}.png`)}/>
     <strong>{fish.name}</strong>
-    <div><i className="material-icons">flash_on</i>  {fish.power}</div>
     <div><i className="material-icons">healing</i> {fish.hp}</div>
+    <div><i className="material-icons">flash_on</i>  {fish.power}</div>
   </div>
 )
 }
